c47cfb3aa6ab2f48d976d9f6d1a59b73095c6360
dyingc <dyingc@alumni.sjtu.edu.cn>
2023-11-26 10:55:24 -0800

    Change folder name to remove space so that the C program can be compiled

---
+++ Part_01/01_Introduction_to_ftrace/14_Tracing_a_specific_process/traceme_with_filter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DEBUGFS=`grep debugfs /proc/mounts | awk '{print $2;}'`
+echo nop > $DEBUGFS/tracing/current_tracer
+echo > $DEBUGFS/tracing/trace
+echo $1
+echo $$ > $DEBUGFS/tracing/set_ftrace_pid
+echo function > $DEBUGFS/tracing/current_tracer
+echo vfs_* > /sys/kernel/debug/tracing/set_ftrace_filter
+echo 1 > $DEBUGFS/tracing/tracing_on
+exec $*
+echo 0 > $DEBUGFS/tracing/tracing_on
