6d9665c60b0bd87087ebef0272618f04e610d9df
dyingc <dyingc@alumni.sjtu.edu.cn>
2023-11-26 21:28:47 -0800

    Finished 15 - Tracing functions of specific module

---
+++ Part_01/01_Introduction_to_ftrace/15_Tracing_functions_of_specific_module/insmod.sh
@@ -0,0 +1,2 @@
+rmmod hello 2>/dev/null
+insmod hello.ko && cat /sys/kernel/tracing/available_filter_functions | egrep '(device|dyingc)_.*\[hello\]' | awk '{print $1}' > /sys/kernel/tracing/set_ftrace_filter
--- Part_01/01_Introduction_to_ftrace/15_Tracing_functions_of_specific_module/traceme.sh
+++ Part_01/01_Introduction_to_ftrace/15_Tracing_functions_of_specific_module/traceme.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+user=$(whoami)
+if [ "${user}" != root ]; then
+    echo "Usage: sudo $0"
+    exit 1
+fi
+
 DEBUGFS=`grep debugfs /proc/mounts | awk '{print $2;}'`
 echo 0 > $DEBUGFS/tracing/tracing_on
 echo nop > $DEBUGFS/tracing/current_tracer
@@ -7,7 +13,12 @@ echo > $DEBUGFS/tracing/trace
 echo  > $DEBUGFS/tracing/set_ftrace_pid
 echo function > $DEBUGFS/tracing/current_tracer
 echo ':mod:e1000' > /sys/kernel/debug/tracing/set_ftrace_filter
+echo ':mod:hello' > /sys/kernel/debug/tracing/set_ftrace_filter
+make > /dev/null
 echo 1 > $DEBUGFS/tracing/tracing_on
-read enter
+#read enter
+./userapp
 echo 0 > $DEBUGFS/tracing/tracing_on
+make clean > /dev/null
 
+cat $DEBUGFS/tracing/trace
