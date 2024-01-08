abdc24c1097db1c659286b1012e1fa3142b88c0e
dyingc <dyingc@alumni.sjtu.edu.cn>
2024-01-07 17:19:21 -0800

    add make.sh

--- samples/kprobe/make.sh
+++ samples/kprobe/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make && make bind_test && ssh dev "rm -f ~/udemy/kernel/UdemyCourseKernelDebug/samples/kprobe/*"; scp -r * dev:~/udemy/kernel/UdemyCourseKernelDebug/samples/kprobe/
+make clean; make && make bind_test && ssh dev "rm -f $PWD/*"; scp -r * dev:$PWD/
